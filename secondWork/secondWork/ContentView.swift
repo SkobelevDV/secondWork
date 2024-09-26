6aaccc3838002de3a61a6ed1548aeee45da5a247
SkobelevDV <skobelevdv@inbox.ru>
2024-09-26 11:15:08 +0300

    comment

---
+++ secondWork/secondWork/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  secondWork
+//
+//  Created by User on 26.09.2024.
+//  Copyright © 2024 User. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ secondWork/secondWorkTests/secondWorkTests.swift
@@ -0,0 +1,34 @@
+//
+//  secondWorkTests.swift
+//  secondWorkTests
+//
+//  Created by User on 26.09.2024.
+//  Copyright © 2024 User. All rights reserved.
+//
+
+import XCTest
+@testable import secondWork
+
+class secondWorkTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
